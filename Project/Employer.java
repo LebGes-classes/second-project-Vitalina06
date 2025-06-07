a7fc3b3d2b743f5c54772e86f93bf049e0252299
Виталина <n.vitalina06@yandex.ru>
2025-06-07 15:33:47 +0300

    Project

---
+++ Project/Employer.java
@@ -0,0 +1,14 @@
+package Project;
+
+public class Employer {
+
+    private final String name;
+
+    public Employer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
